46a72bc291bc8a55dcfdf388576540dbd7d2c0db
Pasquale Aragosa <aragosap@gmail.com>
2022-10-11 15:34:54 +0200

    add append properties

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,28 +15,28 @@ const buttonEl = document.querySelector('button');
 buttonEl.addEventListener('click',
     function () {
         const userEmail = document.getElementById('email').value;
+        const boxEl = document.querySelector('check');
+        //console.log(boxEl);
         let issue = `Account inesistente!`;
+
         for (let i = 0; i < emailList.length; i++) {
             if (emailList[i] === userEmail) {
                 issue = `Log-in effettuato!`;
             }
         }
-        alert(issue);
-        // let divEl = document.querySelector("div");
-        // let pEl = document.createElement("p");
-        // divEl.append(pEl);
-        // console.log(divEl);
-        // pEl.innerHTML = issue;
+        //alert(issue);
+        boxEl.append(issue);
+        
         const userNum = Math.floor((Math.random() * 6) + 1);
         const pcNum = Math.floor((Math.random() * 6) + 1);
-        //console.log(userNum, pcNum);
+        
         
         if (userNum > pcNum) {
-            alert('Hai vinto!');
+            //alert('Hai vinto!');
         } else if (pcNum > userNum) {
-            alert('Hai perso!');
+            //alert('Hai perso!');
         } else {
-            alert('Pareggio');
+            //alert('Pareggio');
         }
     }
 )
